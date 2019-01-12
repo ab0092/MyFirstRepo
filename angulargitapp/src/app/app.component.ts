7b3a3a50a23057627ff1ea050179968b3e8400a9
ab0092 <abhishekbag0092@gmail.com>
2019-01-12 18:30:07 +0530

    App Component TS modified

--- angulargitapp/src/app/app.component.ts
+++ angulargitapp/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  /**
+   *
+   */
+  constructor() {
+    console.log("HELLO WORLD");    
+  }
   title = 'app';
 }
